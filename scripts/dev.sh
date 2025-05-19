7db27d86a168fc203043e939474707ab2963b9f8
daniel calderon <danieleduardocald@gmail.com>
2025-05-19 13:02:57 -0400

    source code

--- scripts/dev.sh
+++ scripts/dev.sh
@@ -6,7 +6,7 @@ export DD_ENV=apm-test
 export DD_VERSION=1.0
 export DD_GIT_REPOSITORY_URL=github.com/ccdaniele/se-ssi-playground
 export DD_TRACE_AGENT_PORT=8136
-export NODE_OPTIONS="--require dd-trace/init"
+export NODE_OPTIONS="--require dd-trace/init --enable-source-maps flag"
 export DD_TRACE_DEBUG=true
-export DD_GIT_COMMIT_SHA=525a1f1b5875367e7c12b2423527d221e1aa533c
-next dev
+export DD_GIT_COMMIT_SHA="525a1f1b5875367e7c12b2423527d221e1aa533c"
+next dev 
